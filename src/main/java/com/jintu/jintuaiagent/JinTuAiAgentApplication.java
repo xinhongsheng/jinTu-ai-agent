cd6d473340fdcacb9a7eee503c705863c760a0e3
xinhongsheng <https://gitee.com/xinhongsheng>
2026-01-05 12:37:59 +0800

    初始化

---
+++ src/main/java/com/jintu/jintuaiagent/JinTuAiAgentApplication.java
@@ -0,0 +1,13 @@
+package com.jintu.jintuaiagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JinTuAiAgentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JinTuAiAgentApplication.class, args);
+    }
+
+}
