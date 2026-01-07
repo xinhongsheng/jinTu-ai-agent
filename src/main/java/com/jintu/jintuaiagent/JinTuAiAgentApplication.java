deabe6012308e9fbcd21764c0cf821f2c014ee13
xinhongsheng <https://gitee.com/xinhongsheng>
2026-01-07 16:39:57 +0800

    添加阿里云向量数据库

--- src/main/java/com/jintu/jintuaiagent/JinTuAiAgentApplication.java
+++ src/main/java/com/jintu/jintuaiagent/JinTuAiAgentApplication.java
@@ -1,9 +1,10 @@
 package com.jintu.jintuaiagent;
 
+import org.springframework.ai.autoconfigure.vectorstore.pgvector.PgVectorStoreAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = PgVectorStoreAutoConfiguration.class)
 public class JinTuAiAgentApplication {
 
     public static void main(String[] args) {
